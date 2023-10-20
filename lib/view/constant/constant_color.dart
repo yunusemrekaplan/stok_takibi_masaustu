e9a7e8ec8d054204de69f0b7a7692c270db347f5
yunusemrekaplan <yunusemrekaplan1@hotmail.com>
2023-10-20 23:08:14 +0300

    .

--- lib/view/constant/constant_color.dart
+++ lib/view/constant/constant_color.dart
@@ -6,11 +6,17 @@ const Color focusedColor = Colors.green;
 const Color errorColor = Colors.red;
 const Color iconColor = Colors.white;
 const Color transparent = Colors.transparent;
+const Color scrollbarThumbColor = Colors.white38;
+
 const Color firstThemeAppBarColor = Color(0xFF1F3F65);
 const Color firstThemeButtonColor = Color(0xFF043762);
 const Color firstThemeScaffoldBackgroundColor = Color(0xFF34537F);
 const Color firstThemeContainerColor = Color(0xFF2F3F63);
+const Color firstThemeDataTableHeadingRowColor = Color(0xFF043762);
+
+
 const Color secThemeAppBarColor = Color(0xFF0F1432);
 const Color secThemeButtonColor = Color(0xFF3D4574);
 const Color secThemeScaffoldBackgroundColor = Color(0xFF1C2039);
 const Color secThemeContainerColor = Color(0xFF262B49);
+const Color secThemeDataTableHeadingRowColor = Color(0xFF3D4574);
